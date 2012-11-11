9675876e51fb6a0ba4849fd95ff496f83acf4143
Ando Yasushi <andyjpn@gmail.com>
2012-11-11 11:43:43 +0900

    modify option -t

--- lib/sapphire.rb
+++ lib/sapphire.rb
@@ -2,25 +2,29 @@ require 'sapphire/converter'
 
 module Sapphire
   VERSION = '0.0.1'
+  DEFAULT_GENERATOR = 'sapphire/perl_generator'
 
-  def self.convert(string_or_file)
+  def self.convert(string_or_file, type=nil)
+    type ||= DEFAULT_GENERATOR
     if string_or_file.is_a? File
-      self.convert_file string_or_file
+      self.convert_file string_or_file, type
     else
-      self.convert string_or_file
+      self.convert string_or_file, type
     end
   end
 
-  def self.convert_string(string)
-    Converter.new.convert string
+  def self.convert_string(string, type=nil)
+    type ||= DEFAULT_GENERATOR
+    Converter.new.convert string, type
   end
 
-  def self.convert_file(string_or_file)
+  def self.convert_file(string_or_file, type=nil)
+    type ||= DEFAULT_GENERATOR
     if string_or_file.is_a? File or string_or_file.is_a? ARGF.class
-      Converter.new.convert_file string_or_file
+      Converter.new.convert_file string_or_file, type
     else
       string_or_file.open do |file|
-        Converter.new.convert_file file
+        Converter.new.convert_file file, type
       end
     end
   end
--- lib/sapphire/converter.rb
+++ lib/sapphire/converter.rb
@@ -1,16 +1,16 @@
 require 'sapphire/parser'
-require 'sapphire/perl_generator'
-require 'mixi_perl_generator'
 
 module Sapphire
   class Converter
-    def convert(string)
-      generator = PerlGenerator.new(Parser.new.parse string)
+    def convert(string, type)
+      require type
+      class_name = type.split('/').map{|e| e.capitalize.gsub(/_([a-z])/){$1.upcase}}.join '::'
+      generator = eval(class_name).new(Parser.new.parse string)
       generator.generate
     end
 
-    def convert_file(file)
-      convert file.read
+    def convert_file(file, type)
+      convert file.read, type
     end
   end
 end
