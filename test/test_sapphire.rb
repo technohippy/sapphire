ac590d7fc2e785a925aa35c4f7f981d63d0c06b5
Ando Yasushi <andyjpn@gmail.com>
2012-11-13 22:26:35 +0900

    rm unused test

--- test/test_sapphire.rb
+++
@@ -1,8 +0,0 @@
-require "test/unit"
-require "sapphire"
-
-class TestSapphire < Test::Unit::TestCase
-  def test_convert_string
-    assert_equal '1 + 1', Sapphire.convert_string('1 + 1')
-  end
-end
